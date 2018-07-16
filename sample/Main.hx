74a0efc28195720d60a18d95b8260577b6c4a667
flashultra <flashultra@users.noreply.github.com>
2018-07-16 15:30:34 +0300

    Return number between 1 to 6 simulate rolling dice

---
+++ sample/Main.hx
@@ -0,0 +1,12 @@
+import haxe.ds.Map;
+
+
+
+class Main {
+
+    static function main() {
+        var pg: PCG32 = new PCG32();
+        var rnd : Int = pg.random(6)+1;
+        trace("Dice: "+rnd);
+    } 
+}
